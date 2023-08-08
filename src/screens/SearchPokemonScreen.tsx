b645af464ce1a9fd8d93ea5b4aac383d26d1f3d6
Oliver Visoso <ovisoso@ucol.mx>
2023-08-07 21:53:18 -0600

    Search Component

--- src/screens/SearchPokemonScreen.tsx
+++ src/screens/SearchPokemonScreen.tsx
@@ -1,10 +1,19 @@
-import {View, Text} from 'react-native';
+import {View, Platform} from 'react-native';
 import React from 'react';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import SearchInput from '../components/SearchInput';
 
 const SearchPokemonScreen = () => {
+  const {top} = useSafeAreaInsets();
+
   return (
-    <View>
-      <Text>SearchPokemonScreen</Text>
+    <View
+      style={{
+        flex: 1,
+        top: Platform.OS === 'ios' ? top : top + 10,
+        marginHorizontal: 20,
+      }}>
+      <SearchInput />
     </View>
   );
 };
